06dfa5075d8eae56b0887416c85cdf7716308e84
José Augusto <104844662+AugustoOliveira099@users.noreply.github.com>
2024-05-16 03:21:15 +0000

    Adiciona script que analisa notícias

---
+++ analisa_csv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv('first_news.csv')
+header = df.iloc[0]
+news_df = df.iloc[1:]
+
+filtered_news_df = news_df[(news_df['target'] != 'Ciências') & 
+                           (news_df['target'] != 'Vagas') & 
+                           (news_df['target'] != 'Eventos') & 
+                           (news_df['target'] != 'Informes')]
+
+print(filtered_news_df)
+
+count_targets = news_df['target'].value_counts()
+
+print(count_targets)
+
+print(len(news_df['target']))
